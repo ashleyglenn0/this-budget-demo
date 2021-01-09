30f5d35d7c72eaaa60a0046fbe60053ab2789fb8
Ashley Glenn <ashleyglenn@Ashleys-MBP-2.attlocal.net>
2021-01-08 22:12:40 -0600

    added click listeners to buttons

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { BudgetService} from './budget.service';
 import { BudgetItem } from './budgetItem.model';
 import { Budget } from './budget.model';
@@ -10,14 +10,27 @@ import { Budget } from './budget.model';
   providers: [BudgetService]
 })
 export class BudgetComponent implements OnInit {
+  addToBudget = new EventEmitter<{budget: Budget}>();
+  addBudgetItem = new EventEmitter<{budget: Budget, budgetItem: BudgetItem}>();
   budgetItems: BudgetItem[] = [
     new BudgetItem('Ameren', '888-222-1111', 'Payment', 200.00, '1-4-2021')
   ];
   budgets: Budget[] = [
-    new Budget ('Budget1', 'Bills Budget', ['are'])
+    new Budget ('Budget1', 'Bills Budget', this.budgetItems)
   ];
   constructor(private budgetService: BudgetService) {
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
+  addNewBudgetItem(): void{
+    this.addBudgetItem.emit();
+  }
+
+  addNewBudget(): void{
+    this.addToBudget.emit();
+  }
+
+
 }
