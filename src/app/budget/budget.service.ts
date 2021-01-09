a84b4fc1f4d0570b373a03ae5437c083252ba12c
Ashley Glenn <ashleyglenn@Ashleys-MBP-2.attlocal.net>
2021-01-08 21:15:39 -0600

    updated service for budget and added budget methods

--- src/app/budget/budget.service.ts
+++ src/app/budget/budget.service.ts
@@ -1,15 +1,37 @@
 import { BudgetItem } from './budgetItem.model';
 import { Budget } from './budget.model';
+import { Injectable} from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
 
 export class BudgetService {
-  constructor(budget: Budget) {
-  }
+  budgetItems: BudgetItem[];
+  budgets: Budget[];
+  constructor() {
+    this.budgetItems = [
+      new BudgetItem('Spire', '1-888-555-1212', 'Payment', 200.00, '1/4/2021')
+    ];
 
-  onAddBudgetItem(budgetItem: BudgetItem){
-    this.budgetItems.push(budgetItem);
+    this.budgets = [
+      new Budget('Budget1', 'Home Budget', this.budgetItems)
+    ];
   }
-  onAddToBudget(budget: Budget){
-    this.budget.push(budgets);
+ addNewBudgetItem(budget: Budget, budgetItem: BudgetItem): void {
+    for (let i = 0; i < this.budgets.length; i++) {
+      if (budget.id === this.budgets[i].id) {
+        this.budgets[i].budgetItems.push(budgetItem);
+      }
+    }
+    this.budgetItems.push(budgetItem);
+ }
+  addNewBudget(budget: Budget): void {
+    this.budgets.push(budget);
+  }
+  getBudgets(): Observable<Budget[]> {
+    return of(this.budgets);
   }
 
 }
--- src/app/budget/budgetlists/allbudgets/allbudgets.component.ts
+++ src/app/budget/budgetlists/allbudgets/allbudgets.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Budget } from '../../budget.model';
+import { BudgetService } from '../../budget.service';
 
 @Component({
   selector: 'app-allbudgets',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./allbudgets.component.scss']
 })
 export class AllbudgetsComponent implements OnInit {
-  budgets = [];
-  constructor() { }
+  budgets: Budget[] = [];
+
+  constructor(private budgetService: BudgetService) { }
 
   ngOnInit(): void {
+    this.getBudgets();
+  }
+  getBudgets(): void{
+    this.budgetService.getBudgets().subscribe(budgets => this.budgets = budgets);
   }
 
 }
