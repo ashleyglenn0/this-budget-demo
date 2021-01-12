c1081c5af35a2fb17cc0041840b4422e2e25a359
Ashley Glenn <ashleyglenn@Ashleys-MBP-2.attlocal.net>
2021-01-12 00:20:07 -0600

    updated service to be able to add budget items

--- src/app/budget.service.ts
+++ src/app/budget.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Budget } from './budget/budget.model';
+import { BudgetItem } from './budget/budgetItem.model';
 
 
 @Injectable({
@@ -7,13 +8,22 @@ import { Budget } from './budget/budget.model';
 })
 export class BudgetService {
   budgets: Budget[];
+  budgetItems: BudgetItem[];
   constructor() { this.budgets = [
     new Budget('Budget1', 'Home Budget', [])
-  ];}
-  createBudget(budgetName: string, budgetDescription: string):number{
+  ];
+                  this.budgetItems = [
+    new BudgetItem('Ameren Electric', '888-222-2222', 'payment', 200.00, '1/11/21')
+  ]; }
+  createBudget(budgetName: string, budgetDescription: string): number{
     const budget: Budget = new Budget(budgetName, budgetDescription, []);
     this.budgets.push(budget);
     return budget.id;
+  }
+  createBudgetItem(date: string, companyName: string, companyPhoneNumber: string, type: string, amount: number): any{
+    const budgetItem: BudgetItem = new BudgetItem(companyName, companyPhoneNumber, type, amount, date);
+    this.budgetItems.push(budgetItem);
+    return budgetItem.id;
   }
   getBudgets(): Budget[] {
     return this.budgets;
--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,37 +1,29 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { BudgetServiceOriginal} from './budget.service.original';
-import { BudgetItem } from './budgetItem.model';
-import { Budget } from './budget.model';
+import { Component, OnInit } from '@angular/core';
+import { BudgetService } from '../budget.service';
+import {FormGroup, NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.scss'],
-  providers: [BudgetServiceOriginal]
+  providers: [BudgetService]
 })
 export class BudgetComponent implements OnInit {
-  addToBudget = new EventEmitter<{budget: Budget}>();
-  addBudgetItem = new EventEmitter<{budget: Budget, budgetItem: BudgetItem}>();
-  budgetItems: BudgetItem[] = [
-    new BudgetItem('Ameren', '888-222-1111', 'Payment', 200.00, '1-4-2021')
-  ];
-  budgets: Budget[] = [
-    new Budget ('Budget1', 'Bills Budget', this.budgetItems),
-    new Budget ('Budget2', 'Savings Budget', this.budgetItems)
-  ];
-  constructor(private budgetService: BudgetServiceOriginal) {
-  }
+
+  constructor(private budgetService: BudgetService) { }
 
   ngOnInit(): void {
-
-  }
-  addNewBudgetItem(): void{
-    this.addBudgetItem.emit();
+    console.log(this.budgetService);
   }
 
-  addNewBudget(): void{
-    this.addToBudget.emit();
+  onSubmit(form: NgForm): any{
+    console.log(form.form.value.budgetItems);
+    const date = form.form.value.date;
+    const companyName = form.form.value.companyName;
+    const companyPhoneNumber = form.form.value.companyPhone;
+    const type = form.form.value.type;
+    const amount = form.form.value.amount;
+    const budgetItemsId = this.budgetService.createBudgetItem(date, companyName, companyPhoneNumber, type, amount);
   }
-
-
 }
