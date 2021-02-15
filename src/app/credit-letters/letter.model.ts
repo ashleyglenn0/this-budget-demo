1185e08ceb8224f637cd601fac6a358c25106975
Ashley Glenn <arglenn023@gmail.com>
2021-02-15 12:06:01 -0600

    added fonts and more styling for components

--- src/app/credit-letters/letter.model.ts
+++ src/app/credit-letters/letter.model.ts
@@ -2,7 +2,7 @@ import { identifierModuleUrl } from "@angular/compiler";
 
 export class CreditLetters {
   public id: number;
-  public name: string;
+  public letterName: string;
   public description: string;
   public companyName: string;
   public companyPhone: string;
@@ -11,9 +11,9 @@ export class CreditLetters {
   public dateReceived: string;
   public letterSummary: string;
 
-  constructor(name: string, description: string, companyName: string, companyPhone: string, type: string, dateSent: string, dateReceived: string, letterSummary:string){
+  constructor(letterName: string, description: string, companyName: string, companyPhone: string, type: string, dateSent: string, dateReceived: string, letterSummary:string){
     this.id = Date.now();
-    this.name = name;
+    this.letterName = letterName;
     this.description = description;
     this.companyName = companyName;
     this.companyPhone = companyPhone;
