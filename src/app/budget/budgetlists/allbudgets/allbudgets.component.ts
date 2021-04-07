07fba8789fc18817e829fc0ef300d1810aa73b4c
Ashley Glenn <arglenn023@gmail.com>
2021-04-06 21:59:18 -0500

    added update modal

--- src/app/budget/budgetlists/allbudgets/allbudgets.component.ts
+++ src/app/budget/budgetlists/allbudgets/allbudgets.component.ts
@@ -7,6 +7,7 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 import { Observable } from 'rxjs';
 
 
+
 @Component({
   selector: 'app-allbudgets',
   templateUrl: './allbudgets.component.html',
@@ -17,39 +18,40 @@ export class AllbudgetsComponent implements OnInit {
   budgets!: Observable<Budget[]>
   uid: any;
   singleBudget: any;
-  budgetId: any;
- 
-  
+  wantsToDelete = true;
+
+
 
   constructor(private budgetService: BudgetService, private router: Router, private afs: AngularFirestore, private auth: AuthService) { }
 
   ngOnInit(): any {
-    
-    this.auth.getUserState().subscribe(user =>{
-    this.uid = user?.uid;
-    
 
-      // this.budgetService = this.afs.collection(`Users/${this.uid}/budgets`).subscribe();
+    this.auth.getUserState().subscribe(user => {
+      this.uid = user?.uid;
+
+
+
       this.budgetsCollection = this.afs.collection<Budget>(`Users/${this.uid}/budgets`);
-      this.budgets = this.budgetsCollection.valueChanges({idField: 'id'});
-     
-    //    this.budgetsCollection = this.afs.collection(`Users/${this.uid}/budgets`); 
-    //  this.afs.collection(`Users/${this.uid}/budgets`).valueChanges();
+      this.budgets = this.budgetsCollection.valueChanges({ idField: 'id' });
 
 
-  })
-    
-    
+    })
+
+
   }
 
 
-  onViewBudget(budget: Budget): void{
+  onViewBudget(budget: Budget): void {
     this.router.navigate(['/singleBudgetPage', budget.id]);
   }
 
-  onDeleteBudget(budget: Budget): any{  
-  this.afs.collection(`Users/${this.uid}/budgets`).doc(budget.id).delete();
-  
+  onDeleteBudget(budget: Budget): any {
+    confirm('Are you sure you want to delete this budget?');
+    this.afs.collection(`Users/${this.uid}/budgets`).doc(budget.id).delete();
+  }
+  onUpdateBudget(budget: Budget): any {
+    this.singleBudget = this.afs.collection(`Users/${this.uid}/budgets`).doc(budget.id).update({});
+    
   }
 
 }
